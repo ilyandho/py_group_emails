cba9a78eb4777db5e4827f3f8899ea6590c445f2
ilyandho <ii.ndhote@gmail.com>
2019-05-25 20:19:22 +0300

    Added headers to each message by sender

--- py_group_emails.py
+++ py_group_emails.py
@@ -4,7 +4,11 @@ except:
     print('Bad File')
     exit()
 hist = dict()
+listd = list()
+print(listd)
 for line in fhand:
+
+    listd.append(line)
     # Check to see if the line starts with From
     # Check if the does not only have From and email
     if line.startswith('From') and len(line.split()) > 2:
@@ -15,6 +19,7 @@ for line in fhand:
         # Else increment it
         else:
             hist[day] += 1
+
 # Sort the histogram by days of the week
 keys = hist.keys()
 keys = sorted(keys)
@@ -22,3 +27,12 @@ print(keys)
 
 for key in keys:
     print(key, ':', hist[key])
+
+# Group emails
+print('Grouped emails')
+
+for line in listd:
+    if line.startswith('From') and len(line.split()) > 2:
+        print('\n\t********', line.strip(), '********\n')
+    else:
+        print(line.strip())
